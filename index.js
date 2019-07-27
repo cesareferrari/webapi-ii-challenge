b5fc4c58b47ddc83923d73bf01d6ceb5a17ad8a5
Cesare Ferrari <cesare@maryandferrari.com>
2019-07-27 14:04:46 -0400

    Use Express router

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const express = require('express');
-const server = express();
-const db = require("./data/db.js");
-server.use(express.json());
+const server = require('./server.js');
 
 server.listen(4000, () => {
   console.log("Server listening on port 4000.");
 });
 
-server.get('/', (req, res) => {
-  res.send(
-    "<h1>Welcome to the API</h1>"
-  )
-});
-
-server.get('/api/posts', (req, res) => {
-  db.find()
-    .then(posts => {
-      res.status(200).json(posts);
-    })
-    .catch(err => {
-      res.status(500).json({success: false, err});
-    })
-})
