19dc65f86caa78af2b732b237c734ac2b76a921d
Cesare Ferrari <cesare@maryandferrari.com>
2019-07-30 21:44:42 -0400

    Add new post

--- data/posts-router.js
+++ data/posts-router.js
@@ -15,11 +15,10 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const post = await Posts.findById(req.params.id);
-    console.log('Post is:', post[0]);
+    const posts = await Posts.findById(req.params.id);
 
-    if (post[0]) {
-      res.status(200).json(post);
+    if (posts[0]) {
+      res.status(200).json(posts[0]);
     } else {
       res.status(404).json({ message: 'The post with the specified ID does not exist.' });
     }
@@ -27,6 +26,21 @@ router.get('/:id', async (req, res) => {
     res.status(500).json({ error: "The post information could not be retrieved." });
   }
 });
+
+router.post('/', async (req, res) => {
+  try {
+    console.log('Req.body', req.body);
+    if (req.body.title && req.body.contents) {
+      const post = await Posts.insert(req.body);
+      const newPost = await Posts.findById(post.id);
+      res.status(201).json(newPost);
+    } else {
+      res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "There was an error while saving the post to the database" });
+  }
+})
 
 
 module.exports = router;
