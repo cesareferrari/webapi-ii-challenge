8c3d299fbb842bf7ee5b327a1015c75aae838fb0
Cesare Ferrari <cesare@maryandferrari.com>
2019-07-30 21:59:39 -0400

    Delete post

--- data/posts-router.js
+++ data/posts-router.js
@@ -40,7 +40,25 @@ router.post('/', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "There was an error while saving the post to the database" });
   }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    console.log(post[0]);
+    if (post[0]) {
+      const removedPost = await Posts.remove(post.id);
+      res.status(200).json({ message: "Post was removed" })
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "The post could not be removed" });
+  }
 })
+
+
 
 
 module.exports = router;
