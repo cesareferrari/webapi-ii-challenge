29fb2f5bdfa1e9f32edf0aed6105b2d78735fd31
Cesare Ferrari <cesare@maryandferrari.com>
2019-07-30 21:07:21 -0400

    Retrieve one post

--- data/posts-router.js
+++ data/posts-router.js
@@ -8,8 +8,25 @@ router.get('/', async (req, res) => {
     res.status(200).json(posts);
   } catch (error) {
     console.log(error);
-    res.status(500).json({message: 'Error retrieving the posts'});
+    res.status(500).json({error: "The posts information could not be retrieved."});
   }
 });
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    console.log('Post is:', post[0]);
+
+    if (post[0]) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "The post information could not be retrieved." });
+  }
+});
+
 
 module.exports = router;
